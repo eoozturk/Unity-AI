689a81f476e464a65ffdaa1c02d946b613f55fef
Emre {Ozan} Öztürk <eoozturk.dev@gmail.com>
2022-12-03 18:23:03 +0300

    Player and Opponent Controller
    Player and opponent control script, speedboost, ıenumerator and coroutine added

--- UnityAI/Assets/Scripts/CheckCollisions.cs
+++ UnityAI/Assets/Scripts/CheckCollisions.cs
@@ -5,30 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class CheckCollisions : MonoBehaviour
 {
-	public GameObject RestartPanel;
+	float speedBoost = 3.0f;
+	Vector3 playerStartPos;
+	public GameObject RestartPanel, sBoostController;
 	public PlayerController playerController;
 
-	public void RestartGame()
+    private void Start()
+    {
+		playerStartPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    public void RestartGame()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-
-		playerController.runningSpeed = 0f;
-		//playerController.PlayerAnim.SetBool("win", true);
-		transform.Rotate(transform.rotation.x, 180, transform.rotation.z, Space.Self);
-		RestartPanel.SetActive(true);
-	
+        if (other.CompareTag("End"))
+        {
+			playerController.runningSpeed = 0f;
+			//playerController.PlayerAnim.SetBool("win", true);
+			transform.Rotate(transform.rotation.x, 180, transform.rotation.z, Space.Self);
+			RestartPanel.SetActive(true);
+		}
+		if (other.CompareTag("SpeedBoost"))
+		{
+			sBoostController.SetActive(true);
+			playerController.runningSpeed = playerController.runningSpeed + speedBoost;
+			StartCoroutine(SpeedBoostControl());
+		}
 	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
 		if (collision.collider.CompareTag("Obstacle"))
 		{
+			transform.position = playerStartPos;
 			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
 	}
 
+	private IEnumerator SpeedBoostControl()
+    {
+		yield return new WaitForSeconds(2.0f);
+		sBoostController.SetActive(false);
+		playerController.runningSpeed = playerController.runningSpeed - speedBoost;
+    }
 }
---
+++ UnityAI/Assets/Scripts/Opponent.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Opponent : MonoBehaviour
+{
+    float speedBoost = 1.0f;
+    Vector3 opponentStartPos;
+    public GameObject target, sBoostController;
+    public NavMeshAgent opponentAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        opponentAgent = GetComponent<NavMeshAgent>();
+        opponentStartPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        opponentAgent.SetDestination(target.transform.position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Obstacle"))
+        {
+            transform.position = opponentStartPos;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SpeedBoost"))
+        {
+            sBoostController.SetActive(true);
+            opponentAgent.speed = opponentAgent.speed + speedBoost;
+            StartCoroutine(SpeedBoostControl());
+        }
+    }
+
+    private IEnumerator SpeedBoostControl()
+    {
+        yield return new WaitForSeconds(1.0f);
+        sBoostController.SetActive(false);
+        opponentAgent.speed = opponentAgent.speed - speedBoost;
+    }
+}
